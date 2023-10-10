e809692d620f8a0afd7d4973e0f54b92641b167d
Mihouse-1906 <gabriellopesgoncalves189@gmail.com>
2023-10-10 10:53:54 -0300

    P/seletor e f/seletor html/terminar

--- src/app/produtos/produto-listagem/produto-listagem.component.ts
+++ src/app/produtos/produto-listagem/produto-listagem.component.ts
@@ -1,3 +1,4 @@
+import { ProdutosService } from './../../shared/service/produtos.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,22 @@ import { Component } from '@angular/core';
   templateUrl: './produto-listagem.component.html',
   styleUrls: ['./produto-listagem.component.scss']
 })
-export class ProdutoListagemComponent {
+export class ProdutoListagemComponent implements OnInit {
+  public produtos : Array<Produto> = new Array();
 
+  constructor(private produtosService: ProdutosService){
+  }
+  gnOnInit(): void{
+    this.buscarProdutos();
+  }
+  buscarProdutos(){
+    this.produtosService.listarTodos().subscribe(
+    resultado =>{
+      this.produtos = resultado;
+    },
+    error =>{
+      console.log('Erro ao buscar produtos', error);
+      }
+    );
+  }
 }
